9a41c8cda0df52c860e10f834280613c8a2795b5
mall <mallhate@ukr.net>
2014-10-20 03:21:01 +0300

    make relocate after login

--- app/forms/main.py
+++ app/forms/main.py
@@ -6,35 +6,58 @@ import app.db_funcs.main as database
 
 @app.route('/login', methods=['POST'])
 def login():
+    res = {}
 
-    login = request.form.get("user", "").encode("utf-8")
-    passwd = request.form.get("pass", "").encode("utf-8")
+    login = request.form.get("user", None)
+    login = login.encode("utf-8") if login else login
+    passwd = request.form.get("pass", None)
+    passwd = passwd.encode("utf-8") if passwd else passwd
 
-    if not passwd or not login: res = '0'
-    if len(login) < 2 or len(passwd) < 2: res = '0'
+
+    if (not passwd) or (not login):
+        res['error'] = 'Login or password empty'
+        return json.dumps(res)
+
+    if (len(login) < 2) or (len(passwd) < 2):
+        res['error'] = "Login or password haven't right size"
+        return json.dumps(res)
 
     if not database.try_login(login, passwd):
-        res = '0'
+        res['error'] = "Wrong login or password"
     else:
         session['login'] = login
-        res = '1'
+        res['success'] = ""
 
-    return res
+    return json.dumps(res)
+
 
 @app.route('/regist', methods=['POST'])
 def regitration():
-    res = '0'
-    login = request.form.get("user", "").encode("utf-8")
-    passwd = request.form.get("pass", "").encode("utf-8")
-    email = request.form.get("email", "").encode("utf-8")
+    res = {}
 
-    if not passwd or not login: res = '0'
-    if len(login) < 2 or len(passwd) < 2: res = '0'
+    login = request.form.get("user", None)
+    login = login.encode("utf-8") if login else login
 
-    if not database.add_new_user(login, passwd, email): res = '0'
-    else: res = '1'
+    passwd = request.form.get("pass", None)
+    passwd = passwd.encode("utf-8") if passwd else passwd
 
-    return res
+    email = request.form.get("email", None)
+    email = email.encode("utf-8") if email else ""
+
+    if (not passwd) or (not login):
+        res['error'] = 'Login or password empty'
+        return json.dumps(res)
+
+    if (len(login) < 2) or (len(passwd) < 2):
+        res['error'] = "Login or password haven't right size"
+        return json.dumps(res)
+
+    if not database.add_new_user(login, passwd, email):
+        res['error'] = "User already exists"
+    else:
+        res['success'] = ""
+
+    return json.dumps(res)
 
 
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,6 +4,7 @@ $(document).keyup(function(e) {
   }
 });
 $(document).ready(function() {
+
   $('a.registration').click(function() {
     $('.registration-form-wrapper').fadeIn();
     $('.popup-background').fadeIn();
@@ -20,14 +21,14 @@ $(document).ready(function() {
     $('.form-wrapper:visible').hide();
     $(this).hide();
   });
-  $('.form-wrapper input[type="submit"]').one().click(function() {
+  $('.registration-form-wrapper input[type="submit"]').one().click(function() {
     var elemName = $(this).siblings('input[name="user"]');
     var elemPass = $(this).siblings('input[name="pass"]');
     var elemRe_pass = $(this).siblings('input[name="re_pass"]');
     var name = $(elemName).val();
     var pass = $(elemPass).val();
     var re_pass = $(elemRe_pass).val();
-    if (name.length < 1) {
+    if (name.length < 2) {
       $('.error-message').remove();
       $(elemName).before('<div class="error-message">field is required</div>');
       return false;
@@ -37,7 +38,7 @@ $(document).ready(function() {
       $(elemName).before('<div class="error-message">name is too long</div>');
       return false;
     }
-    if (pass.length < 1) {
+    if (pass.length < 2) {
       $('.error-message').remove();
       $(elemPass).before('<div class="error-message">field is required</div>');
       return false;
@@ -49,8 +50,68 @@ $(document).ready(function() {
     }
     if (pass != re_pass) {
       $('.error-message').remove();
-      $(elemPass).before('<div class="error-message">passwords do not mutch</div>');
+      $(elemPass).before('<div class="error-message">passwords must match</div>');
       return false;
     }
+  });
+
+
+  $('.login-form-wrapper input[type="submit"]').one().click(function() {
+    var elemName = $(this).siblings('input[name="user"]');
+    var elemPass = $(this).siblings('input[name="pass"]');
+
+    var name = $(elemName).val();
+    var pass = $(elemPass).val();
+
+    if (name.length < 2) {
+      $('.error-message').remove();
+      $(elemName).before('<div class="error-message">field is required</div>');
+      return false;
+    }
+    if (name.length > 32) {
+      $('.error-message').remove();
+      $(elemName).before('<div class="error-message">name is too long</div>');
+      return false;
+    }
+    if (pass.length < 2) {
+      $('.error-message').remove();
+      $(elemPass).before('<div class="error-message">field is required</div>');
+      return false;
+    }
+    if (pass.length > 32) {
+      $('.error-message').remove();
+      $(elemPass).before('<div class="error-message">password is too long</div>');
+      return false;
+    }
+
+
+    var login_form = $.post("/login"
+      ,{
+        user: name,
+        pass: pass
+      }
+      , function(data) {
+        if ("success" in data){
+            location.reload(true);// <!-- relocate to home  -->
+        }
+        else {
+            var mes = "Other error";
+            if ("error" in data)  mes = data["error"];
+            $('.error-message').remove();
+            $(elemPass).before('<div class="error-message">' + mes + '</div>');
+        }
+      }
+      , "json"
+    );
+
+    login_form.fail(
+        function() {
+            var mes = "";
+            $('.error-message').remove();
+            $(elemPass).before('<div class="error-message">Request error</div>');
+        }
+    );
+    return false;
+
   });
 });
