81ef63f39de2259a4fe90439bc68161ac36c4c0f
mall <mallhate@ukr.net>
2014-10-21 00:09:43 +0300

    Add to settings.py: paths.cong parser

--- app/settings.py
+++ app/settings.py
@@ -1,20 +1,36 @@
 import os
 
+def config_parser(f):
+    res = {}
+    lines = f.readlines()
+    for l in lines:
+        pa = [p for p in l.replace('\r\n', '').replace('\n', '').split(':')]
+        if len(pa)>1:
+            key = pa[0]
+            value = ''.join(( pa[i] for i in range(len(pa)) if i>0 ))
+            res[key] = value
 
+    return res
 
-g_vars = dict()
-g_vars['main_path'] = "/home/w123/proj/g_vk"
+default_config = """main_path:/home/w123/proj/g_vk
+mysql_passwd:123456
+mysql_user:root
+mysql_database:g_vk
+mysql_server:localhost
+"""
+
+if (not os.path.exists("paths.conf")):
+    c_file = open("paths.conf", "wt")
+    c_file.writelines(default_config)
+    c_file.close()
+
+c_file = open("paths.conf", "rt")
+g_vars = config_parser(c_file)
+c_file.close()
+
 g_vars['templates_path'] = os.path.join(g_vars['main_path'], 'app/templates')
-
 g_vars['logs_path'] = os.path.join(g_vars['main_path'], "log/events.log")
-
 g_vars['static_images_path'] = os.path.join(g_vars['main_path'], "static/images")
 g_vars['static_css_path'] = os.path.join(g_vars['main_path'], "static/css")
 g_vars['static_js_path'] = os.path.join(g_vars['main_path'], "static/js")
-
-g_vars['mysql_passwd'] = "123456"
-g_vars['mysql_user'] = "root"
-g_vars['mysql_database'] = "g_vk"
-g_vars['mysql_server'] = "localhost"
-
 
