a883201fdaec4425004599c129a1c1462ae8c73d
mall <mallhate@ukr.net>
2014-10-21 11:29:07 +0300

    Add control for upload files

--- run.py
+++ run.py
@@ -14,30 +14,6 @@ import app.db_funcs.main
 
 from app.db_funcs import main as dbs
 
-
-# db_1 = dbs.db_con()
-# cur = db_1.cursor()
-
-# #cur.execute("LOCK TABLES `users` WRITE")
-# for i in xrange(1000000):
-#     cur.execute("INSERT INTO `users` (`name`, `passwd`, `email`, `access_status`) VALUES ('{0}', PASSWORD('{1}'), '{2}', 'user')".format("ololshka_user_#" + str(i), "2222331", "lox1", "user"))
-#     if i % 1000 == 0 :
-#         print str(i)
-#         if i % 10000 == 0:
-#             db_1.commit()
-# #cur.execute("UNLOCK TABLES")
-# db_1.commit()
-
-# cur.close()
-# db_1.close()
-
-
-
-
-
-
-##
-
 if __name__ == "__main__":
     pass
     application.run(debug=True)
